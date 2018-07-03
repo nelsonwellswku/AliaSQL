254c5035ca6738a0b81ce89e224c8ff908d5b37a
Nelson Wells <nelsonwellswku@gmail.com>
2018-07-03 17:53:07 -0500

    more unit tests

---
+++ source/AliaSQL.Test/ConnectionSettingsTests.cs
@@ -0,0 +1,64 @@
+﻿using AliaSQL.Core.Model;
+using FluentAssertions;
+using Xunit;
+
+namespace AliaSQL.Test
+{
+    public class ConnectionSettingsTests
+    {
+        [Fact]
+        public void PropertyAccessorsWork()
+        {
+            // Act
+            var connectionSettings = new ConnectionSettings("server", "database", true, "username", "password");
+
+            // Assert
+            connectionSettings.Server.Should().Be("server");
+            connectionSettings.Database.Should().Be("database");
+            connectionSettings.IntegratedAuthentication.Should().BeTrue();
+            connectionSettings.Username.Should().Be("username");
+            connectionSettings.Password.Should().Be("password");
+        }
+
+        [Fact]
+        public void ProperlyComparesTwoIdenticalConnectionSettings()
+        {
+            // Arrange
+            var connectionSettingsOne = new ConnectionSettings("server", "database", true, "username", "password");
+            var connectionSettingsTwo = new ConnectionSettings("server", "database", true, "username", "password");
+
+            // Act
+            var areEqual = connectionSettingsOne.Equals(connectionSettingsTwo);
+
+            // Assert
+            areEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ProperlyComparesTwoNonIdenticalConnectionSettings()
+        {
+            // Arrange
+            var connectionSettingsOne = new ConnectionSettings("serverOne", "database", true, "username", "password");
+            var connectionSettingsTwo = new ConnectionSettings("serverTwo", "database", true, "username", "password");
+
+            // Act
+            var areEqual = connectionSettingsOne.Equals(connectionSettingsTwo);
+
+            // Assert
+            areEqual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CalculatesCorrectHashCode()
+        { 
+            // Arrange
+            var expectedHashCode = "ServerDatabaseUsernamePasswordTrue".GetHashCode();
+
+            // Act
+            var connectionSettings = new ConnectionSettings("Server", "Database", true, "Username", "Password");
+
+            // Assert
+            connectionSettings.GetHashCode().Should().Be(expectedHashCode);
+        }
+    }
+}
---
+++ source/AliaSQL.Test/ConnectionStringGeneratorTests.cs
@@ -0,0 +1,50 @@
+﻿using AliaSQL.Core.Model;
+using AliaSQL.Core.Services.Impl;
+using FluentAssertions;
+using Xunit;
+
+namespace AliaSQL.Test
+{
+    public class ConnectionStringGeneratorTests
+    {
+        [Fact]
+        public void CorrectlyGeneratesConnectionStringWithDatabaseAndIntegratedSecurity()
+        {
+            var generator = new ConnectionStringGenerator();
+            var settings = new ConnectionSettings("server", "db", true, string.Empty, string.Empty);
+            var connectionString = generator.GetConnectionString(settings, true);
+
+            connectionString.Should().Be("Data Source=server;Initial Catalog=db;Integrated Security=True;");
+        }
+
+        [Fact]
+        public void CorrectlyGeneratesConnectionStringWithDatabaseAndIntegratedSecurityAndDatabaseExcluded()
+        {
+            var generator = new ConnectionStringGenerator();
+            var settings = new ConnectionSettings("server", "db", true, string.Empty, string.Empty);
+            var connectionString = generator.GetConnectionString(settings, false);
+
+            connectionString.Should().Be("Data Source=server;Integrated Security=True;");
+        }
+
+        [Fact]
+        public void CorrectlyGeneratesConnectionStringWithDatabaseAndUserSecurity()
+        {
+            var generator = new ConnectionStringGenerator();
+            var settings = new ConnectionSettings("server", "db", false, "usr", "pwd");
+            var connectionString = generator.GetConnectionString(settings, true);
+
+            connectionString.Should().Be("Data Source=server;Initial Catalog=db;User ID=usr;Password=pwd;");
+        }
+
+        [Fact]
+        public void CorrectlyGeneratesConnectionStringWithDatabaseAndUserSecurityAndDatabaseExcluded()
+        {
+            var generator = new ConnectionStringGenerator();
+            var settings = new ConnectionSettings("server", "db", false, "usr", "pwd");
+            var connectionString = generator.GetConnectionString(settings, false);
+
+            connectionString.Should().Be("Data Source=server;User ID=usr;Password=pwd;");
+        }
+    }
+}
