9b3ffeb90b7980968fa735b27a773e3d59e30a5f
Nelson Wells <nelsonwellswku@gmail.com>
2018-07-05 23:20:47 -0500

    More unit tests

---
+++ source/AliaSQL.Test/DatabaseCreatorTests.cs
@@ -0,0 +1,36 @@
+﻿using AliaSQL.Core;
+using AliaSQL.Core.Model;
+using AliaSQL.Core.Services;
+using AliaSQL.Core.Services.Impl;
+using NSubstitute;
+using Xunit;
+
+namespace AliaSQL.Test
+{
+    public class DatabaseCreatorTests
+    {
+        [Fact]
+        public void CreatesDatabase()
+        {
+            // Arrange
+            var connectionSettings = new ConnectionSettings("server", "db", true, null, null);
+            var taskAttributes = new TaskAttributes(connectionSettings, @"c:\scripts")
+            {
+                RequestedDatabaseAction = RequestedDatabaseAction.Create
+            };
+
+            var queryExecutorMock = Substitute.For<IQueryExecutor>();
+            var scriptFolderExecuturerMock = Substitute.For<IScriptFolderExecutor>();
+            var taskObserver = Substitute.For<ITaskObserver>();
+
+            // Act
+            var creator = new DatabaseCreator(queryExecutorMock, scriptFolderExecuturerMock);
+            creator.Execute(taskAttributes, taskObserver);
+
+            // Assert
+            queryExecutorMock.Received().ExecuteNonQuery(connectionSettings, "create database [db]");
+            taskObserver.Received().Log("Run scripts in Create folder.");
+            scriptFolderExecuturerMock.Received().ExecuteScriptsInFolder(taskAttributes, "Create", taskObserver);
+        }
+    }
+}
---
+++ source/AliaSQL.Test/DatabaseDropperTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AliaSQL.Core.Model;
+using AliaSQL.Core.Services;
+using AliaSQL.Core.Services.Impl;
+using NSubstitute;
+using Xunit;
+
+namespace AliaSQL.Test
+{
+    public class DatabaseDropperTests
+    {
+        [Fact]
+        public void DropsDatabase()
+        {
+            // Arrange
+            var connectionSettings = new ConnectionSettings("server", "db", true, null, null);
+            var taskAttributes = new TaskAttributes(connectionSettings, null);
+
+            var connectionDropperMock = Substitute.For<IDatabaseConnectionDropper>();
+            var taskObserverMock = Substitute.For<ITaskObserver>();
+            var queryExecuterMock = Substitute.For<IQueryExecutor>();
+
+            queryExecuterMock.ReadFirstColumnAsStringArray(connectionSettings, "select @@version").Returns(new[] { "SQL Server" });
+
+            // Act
+            var dropper = new DatabaseDropper(connectionDropperMock, queryExecuterMock);
+            dropper.Execute(taskAttributes, taskObserverMock);
+
+            // Assert
+            taskObserverMock.Received().Log("Running against: SQL Server");
+            taskObserverMock.Received().Log("Dropping database: db\n");
+            connectionDropperMock.Received().Drop(connectionSettings, taskObserverMock);
+            queryExecuterMock.Received().ExecuteNonQuery(connectionSettings, "ALTER DATABASE [db] SET SINGLE_USER WITH ROLLBACK IMMEDIATE drop database [db]");
+        }
+
+        [Fact]
+        public void DropsAzureDatabaseWithoutDroppingConnections()
+        {
+            // Arrange
+            var settings = new ConnectionSettings("server", "db", true, null, null);
+            var taskAttributes = new TaskAttributes(settings, null);
+
+            var connectionDropperMock = Substitute.For<IDatabaseConnectionDropper>();
+            var taskObserverMock = Substitute.For<ITaskObserver>();
+            var queryExecutorMock = Substitute.For<IQueryExecutor>();
+
+            queryExecutorMock.ReadFirstColumnAsStringArray(settings, "select @@version").Returns(new[] {"SQL Azure "});
+
+            // Act
+            var dropper = new DatabaseDropper(connectionDropperMock, queryExecutorMock);
+            dropper.Execute(taskAttributes, taskObserverMock);
+
+            // Assert
+            taskObserverMock.Log("Running against: SQL Azure");
+            taskObserverMock.Log("Dropping database: db\n");
+            queryExecutorMock.Received().ExecuteNonQuery(settings, "drop database [db]");
+            connectionDropperMock.DidNotReceiveWithAnyArgs().Drop(null, null);
+        }
+
+        [Fact]
+        public void ShouldNotFailIfDatabaseDoesNotExist()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
